be5d90b9707f2aff64e1f0351919925bec93bb48
fowwo <54595444+fowwo@users.noreply.github.com>
2020-09-06 14:15:55 -0500

    Solve 2018 Day 5a

---
+++ src/_2018/_5a.java
@@ -0,0 +1,34 @@
+// September 6th, 2020
+package _2018;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class _5a {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File f = new File("input/2018/5.txt");
+            Scanner s = new Scanner(f);
+            String input = s.nextLine();
+            s.close();
+
+            for (int i = 0; i < input.length() - 1; i++) {
+                if (Character.toLowerCase(input.charAt(i)) == Character.toLowerCase(input.charAt(i + 1)) && input.charAt(i) != input.charAt(i + 1)) {
+                    input = input.substring(0, i) + input.substring(i + 2);
+                    i = Math.max(i - 2, -1);
+                }
+            }
+
+            System.out.println(input.length());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}

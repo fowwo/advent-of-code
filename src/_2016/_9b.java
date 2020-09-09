f43bb205fb8a52b78ff995acc5bb127bddbc0f11
fowwo <54595444+fowwo@users.noreply.github.com>
2020-09-09 01:01:35 -0500

    Solve 2016 Day 9b

---
+++ src/_2016/_9b.java
@@ -0,0 +1,50 @@
+// September 9th, 2020
+package _2016;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class _9b {
+
+    public static void main(String[] args) {
+
+        String input = "";
+
+        try {
+
+            File f = new File("input/2016/9.txt");
+            Scanner s = new Scanner(f);
+            input = s.nextLine();
+            s.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(solve(input));
+
+    }
+
+    public static long solve(String s) {
+
+        int value = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                int x = 4;
+                while (s.charAt(i + x) != ')') {
+                    x++;
+                }
+                String[] split = s.substring(i + 1, i + x).split("x");
+                int copySize = Integer.parseInt(split[0]);
+                int copyCount = Integer.parseInt(split[1]);
+                return value + copyCount * solve(s.substring(i + x + 1, i + x + 1 + copySize)) + solve(s.substring(i + x + 1 + copySize));
+            } else {
+                value++;
+            }
+        }
+        return value;
+
+    }
+
+}

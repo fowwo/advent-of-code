03aa5291e5741477c951855fe7ea669e94adbc70
fowwo <54595444+fowwo@users.noreply.github.com>
2020-09-12 16:13:43 -0500

    Solve 2016 Day 18a

---
+++ src/_2016/_18a.java
@@ -0,0 +1,38 @@
+package _2016;
+
+public class _18a {
+
+    public static void main(String[] args) {
+
+        String input = "^^^^......^...^..^....^^^.^^^.^.^^^^^^..^...^^...^^^.^^....^..^^^.^.^^...^.^...^^.^^^.^^^^.^^.^..^.^";
+
+        int total = 0;
+        for (int i = 0; i < 40; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < input.length(); j++) {
+                if (!isTrap(j, input)) total++;
+                if (isNextTrap(j, input)) {
+                    sb.append('^');
+                } else {
+                    sb.append('.');
+                }
+            }
+            input = sb.toString();
+        }
+
+        System.out.println(total);
+
+    }
+
+    public static boolean isNextTrap(int i, String row) {
+        return isTrap(i - 1, row) != isTrap(i + 1, row);
+    }
+
+    public static boolean isTrap(int i, String row) {
+        if (i >= 0 && i < row.length()) {
+            return row.charAt(i) == '^';
+        }
+        return false;
+    }
+
+}

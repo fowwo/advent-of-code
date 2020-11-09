052dd5682993980c4ea29ae9128a1210a3b764a9
fowwo <54595444+fowwo@users.noreply.github.com>
2020-11-09 00:30:01 -0600

    Solve 2017 Day 9b

---
+++ src/_2017/_9b.java
@@ -0,0 +1,50 @@
+// November 8th, 2020
+package _2017;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class _9b {
+
+    public static void main(String[] args) {
+
+        String line = "";
+
+        try {
+
+            File f = new File("input/2017/9.txt");
+            Scanner s = new Scanner(f);
+            line = s.nextLine();
+            s.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Remove garbage
+        int total = 0;
+        int a = -1;
+        for (int i = 0; i < line.length(); i++) {
+            switch (line.charAt(i)) {
+                case '!':
+                    line = line.substring(0, i) + line.substring(i + 2);
+                    i--;
+                    break;
+                case '<':
+                    if (a == -1) a = i;
+                    break;
+                case '>':
+                    line = line.substring(0, a) + line.substring(i + 1);
+                    total += i - a - 1;
+                    i = a - 1;
+                    a = -1;
+                    break;
+            }
+        }
+
+        System.out.println(total);
+
+    }
+
+}

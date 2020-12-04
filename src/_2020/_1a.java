23c6a2f8f6f0352b72c0c9b61ad3e836209d86af
fowwo <54595444+fowwo@users.noreply.github.com>
2020-12-03 22:36:01 -0600

    Solve 2020 Day 1a

---
+++ src/_2020/_1a.java
@@ -0,0 +1,37 @@
+// December 3rd, 2020
+package _2020;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class _1a {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        try {
+
+            File f = new File("input/2020/1.txt");
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) list.add(Integer.parseInt(s.nextLine()));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == 2020) {
+                    System.out.println(list.get(i) * list.get(j));
+                    System.exit(0);
+                }
+            }
+        }
+
+    }
+
+}

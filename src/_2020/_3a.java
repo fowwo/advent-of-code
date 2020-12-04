7d4db1a37d5e49e391b63a744c380cd84383e778
fowwo <54595444+fowwo@users.noreply.github.com>
2020-12-03 22:54:37 -0600

    Solve 2020 Day 3a

---
+++ src/_2020/_3a.java
@@ -0,0 +1,49 @@
+// December 3rd, 2020
+package _2020;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class _3a {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[0][];
+
+        try {
+
+            File f = new File("input/2020/3.txt");
+            Scanner s = new Scanner(f);
+            List<String> list = new ArrayList<>();
+            while (s.hasNextLine()) list.add(s.nextLine());
+
+            matrix = new int[list.size()][list.get(0).length()];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    if (list.get(i).charAt(j) == '.') matrix[i][j] = 0;
+                    else matrix[i][j] = 1;
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int x = 0;
+        int y = 0;
+        int total = 0;
+
+        while (y < matrix.length) {
+            if (matrix[y][x] == 1) total++;
+            x = (x + 3) % matrix[0].length;
+            y++;
+        }
+
+        System.out.println(total);
+
+    }
+
+}

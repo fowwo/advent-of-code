e682e17be2a1f1af7a594d0e2002181bbea821c6
fowwo <54595444+fowwo@users.noreply.github.com>
2020-12-08 15:51:41 -0600

    Solve 2020 Day 6b

---
+++ src/_2020/_6b.java
@@ -0,0 +1,48 @@
+// December 8th, 2020
+package _2020;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class _6b {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File f = new File("input/2020/6.txt");
+            Scanner s = new Scanner(f);
+
+            int total = 0;
+            int count = 0;
+            int[] arr = new int[26];
+            while (s.hasNextLine()) {
+                String str = s.nextLine();
+                if (!str.equals("")) {
+                    count++;
+                    for (int i = 0; i < str.length(); i++) {
+                        int x = str.charAt(i);
+                        if (x >= 97 && x <= 122) arr[x - 97]++;
+                    }
+                } else {
+                    for (int i = 0; i < 26; i++) {
+                        if (arr[i] == count) total++;
+                        arr[i] = 0;
+                    }
+                    count = 0;
+                }
+            }
+            for (int i = 0; i < 26; i++) {
+                if (arr[i] == count) total++;
+            }
+
+            System.out.println(total);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}

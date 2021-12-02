2fdea12d4d766eb9280abda3243d02c4775f0f2f
fowwo <54595444+fowwo@users.noreply.github.com>
2021-12-01 23:14:00 -0600

    Solve 2021 Day 2b

---
+++ src/_2021/_2b.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+let list = fs.readFileSync("input/2021/2.txt", "utf8").split('\r\n');
+list.pop();
+
+let horizontal = 0;
+let depth = 0;
+let aim = 0;
+for (var i = 0; i < list.length; i++) {
+	let arr = list[i].split(" ");
+	let direction = arr[0];
+	let value = parseInt(arr[1]);
+	switch (direction) {
+		case "forward":
+			horizontal += value;
+			depth += aim * value;
+			break;
+		case "down":
+			aim += value;
+			break;
+		case "up":
+			aim -= value;
+			break;
+	}
+}
+console.log(horizontal * depth);

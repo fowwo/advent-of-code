be7c8a664c3f273f7df838b7fc799f11a5a35cb9
fowwo <54595444+fowwo@users.noreply.github.com>
2021-12-03 11:40:53 -0600

    Solve 2021 Day 3a

---
+++ src/_2021/_3a.py
@@ -0,0 +1,26 @@
+# December 3rd, 2021
+
+file = open("input/2021/3.txt", "r")
+line = file.readline().strip()
+length = len(line)
+count = []
+total = 0
+
+for i in range(length):
+	count.append(0)
+
+while line:
+	total += 1
+	for i in range(length):
+		if line[i] == "1":
+			count[i] += 1
+	line = file.readline().strip()
+
+gamma = 0
+for i in range(length):
+	gamma <<= 1
+	if count[i] > total / 2:
+		gamma += 1
+
+epsilon = 2 ** length - 1 - gamma
+print(gamma * epsilon)

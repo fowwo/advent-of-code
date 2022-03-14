17fb3727910f808b67bcc35c5c85913ff0ccbedf
fowwo <54595444+fowwo@users.noreply.github.com>
2022-03-14 14:00:29 -0500

    Solve 2019 Day 13a

---
+++ src/_2019/_13a.js
@@ -0,0 +1,14 @@
+// March 14th, 2022
+
+const fs = require("fs");
+const Computer = require("./Computer");
+const instructions = fs.readFileSync(`${__dirname}/../../input/2019/13.txt`, "utf8").trim().split(',').map(x => parseInt(x));
+
+const opcode = new Computer(instructions);
+opcode.run();
+
+let blocks = 0;
+for (var i = 2; i < opcode.output.length; i += 3) {
+	if (opcode.output[i] === 2) blocks++;
+}
+console.log(blocks);

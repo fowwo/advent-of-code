fb018307a1f7a90b803f982e92cb295b1f0a6b78
fowwo <54595444+fowwo@users.noreply.github.com>
2022-03-14 22:08:46 -0500

    Solve 2019 Day 15a

---
+++ src/_2019/_15a.js
@@ -0,0 +1,37 @@
+// March 14th, 2022
+
+const fs = require("fs");
+const Computer = require("./Computer");
+const instructions = fs.readFileSync(`${__dirname}/../../input/2019/15.txt`, "utf8").trim().split(',').map(x => parseInt(x));
+
+let computers = [[ new Computer(instructions), 0, 0 ]];
+const known = new Set(["0,0"]);
+let moves = 0;
+while (true) {
+	let temp = [];
+	moves++;
+	for (var [computer, x, y] of computers) {
+		temp.push(...move(x, y + 1, 1));
+		temp.push(...move(x, y - 1, 2));
+		temp.push(...move(x - 1, y, 3));
+		temp.push(...move(x + 1, y, 4));
+	}
+	computers = temp;
+}
+
+function move(x, y, direction) {
+	let list = [];
+	if (!known.has(`${x},${y}`)) {
+		known.add(`${x},${y}`);
+		let nc = new Computer(computer.memory, [direction]);
+		nc.run();
+		let status = nc.output.pop();
+		if (status === 1) {
+			list.push([ nc, x, y ]);
+		} else if (status === 2) {
+			console.log(moves);
+			process.exit(0);
+		}
+	}
+	return list;
+}

0c402dd9c0c56e43cdffa21cb8103d1d2901ac7b
fowwo <54595444+fowwo@users.noreply.github.com>
2022-03-17 23:20:08 -0500

    Solve 2019 Day 21b

---
+++ Solutions/2019/21/b.js
@@ -0,0 +1,27 @@
+// March 17th, 2022
+
+const fs = require("fs");
+const Computer = require("../Computer");
+const instructions = fs.readFileSync(`${__dirname}/input.txt`, "utf8").trim().split(',').map(x => parseInt(x));
+
+const ascii = (x) => { return `${x}\n`.split('').map(x => x.charCodeAt(0)); };
+const computer = new Computer(instructions);
+computer.input.push(...ascii("OR E T")); // Jump early if possible (1)
+computer.input.push(...ascii("OR H T"));
+computer.input.push(...ascii("NOT C J"));
+computer.input.push(...ascii("AND T J"));
+computer.input.push(...ascii("AND J T")); // Reset T
+computer.input.push(...ascii("OR B T")); // Jump early if possible (2)
+computer.input.push(...ascii("OR E T"));
+computer.input.push(...ascii("NOT T T"));
+computer.input.push(...ascii("OR T J"));
+computer.input.push(...ascii("AND D J")); // Always land on hull
+computer.input.push(...ascii("NOT A T")); // Jump over immediate hole
+computer.input.push(...ascii("OR T J"));
+computer.input.push(...ascii("RUN"));
+computer.run();
+console.log(computer.output.at(-1));
+
+function print() {
+	console.log(String.fromCharCode(...computer.output));
+}

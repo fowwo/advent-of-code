5b39c6e5cb0de199c7213d98bfd3e5a9715d1afe
fowwo <54595444+fowwo@users.noreply.github.com>
2022-04-25 15:07:52 -0500

    Solve 2019 Day 22a

---
+++ Solutions/2019/22/a.py
@@ -0,0 +1,20 @@
+# April 6th, 2022
+
+file = open("input.txt", 'r')
+
+deck = [i for i in range(0, 10007)]
+for line in file:
+	line = line.strip()
+	if line.startswith("cut"):
+		value = int(line.split(" ")[-1])
+		deck = deck[value:] + deck[:value]
+	elif line.startswith("deal with"):
+		value = int(line.split(" ")[-1])
+		copy = [x for x in deck]
+		p = 0
+		for i in range(len(deck)):
+			deck[p] = copy[i]
+			p = (p + value) % len(deck)
+	else:
+		deck.reverse()
+print(deck.index(2019))

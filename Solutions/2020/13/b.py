a55f7cdda9a9148e0dc7621d55ec0fc2a8402826
fowwo <54595444+fowwo@users.noreply.github.com>
2022-08-06 15:43:50 -0500

    Solve 2020 Day 13b

---
+++ Solutions/2020/13/b.py
@@ -0,0 +1,18 @@
+# August 6th, 2021
+
+file = open(f"{__file__}/../input.txt", "r")
+file.readline()
+buses = file.readline().strip().split(',')
+indices = [ buses.index(x) for x in buses if x != 'x' ]
+buses = [ int(x) for x in buses if x != 'x' ]
+file.close()
+
+t = 0
+inc = 1
+for i in range(len(buses)):
+	b = buses[i]
+	n = indices[i]
+	while (t + n) % b:
+		t += inc
+	inc *= b
+print(t)

1c4644348052b87c8455b3c5db6679030f4e8092
fowwo <54595444+fowwo@users.noreply.github.com>
2022-08-06 14:54:34 -0500

    Solve 2020 Day 12b

---
+++ Solutions/2020/12/b.py
@@ -0,0 +1,38 @@
+# August 6th, 2021
+
+file = open(f"{__file__}/../input.txt", "r")
+
+sx = 0
+sy = 0
+wx = 10
+wy = 1
+for line in file:
+	action = line[0]
+	magnitude = int(line[1:-1])
+	if (action == 'N'):
+		wy += magnitude
+	elif (action == 'S'):
+		wy -= magnitude
+	elif (action == 'E'):
+		wx += magnitude
+	elif (action == 'W'):
+		wx -= magnitude
+	elif (action == 'F'):
+		sx += magnitude * wx
+		sy += magnitude * wy
+	else:
+		if magnitude == 180:
+			wx *= -1
+			wy *= -1
+		else:
+			t = wx
+			wx = wy
+			wy = t
+			if (action == 'L' and magnitude == 90) or (action == 'R' and magnitude == 270):
+				wx *= -1
+			else:
+				wy *= -1
+
+file.close()
+
+print(abs(sx) + abs(sy))

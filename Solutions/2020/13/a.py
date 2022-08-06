4ebc262c3b5886a4fec1cd4579f5fe6f16392dcb
fowwo <54595444+fowwo@users.noreply.github.com>
2022-08-06 15:07:18 -0500

    Solve 2020 Day 13a

---
+++ Solutions/2020/13/a.py
@@ -0,0 +1,14 @@
+# August 6th, 2021
+
+file = open(f"{__file__}/../input.txt", "r")
+start = int(file.readline().strip())
+buses = [ int(x) for x in file.readline().strip().split(',') if x != 'x' ]
+file.close()
+
+time = start
+while True:
+	for bus in buses:
+		if time % bus == 0:
+			print((time - start) * bus)
+			exit()
+	time += 1

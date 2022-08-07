e5cbfe1ddbf6207e7789165e2ea498165e1663fe
fowwo <54595444+fowwo@users.noreply.github.com>
2022-08-06 20:57:36 -0500

    Solve 2020 Day 15a

---
+++ Solutions/2020/15/a.py
@@ -0,0 +1,11 @@
+# August 6th, 2021
+
+input = [ 1, 0, 16, 5, 17, 4 ]
+
+while len(input) != 2020:
+	if input[-1] not in input[:-1]:
+		input.append(0)
+	else:
+		input.append([ x for x in reversed(input[:-1]) ].index(input[-1]) + 1)
+
+print(input[-1])

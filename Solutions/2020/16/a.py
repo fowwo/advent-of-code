651f07f5cb4bc249c124ef6e37e37628fff17e40
fowwo <54595444+fowwo@users.noreply.github.com>
2022-08-06 21:52:48 -0500

    Solve 2020 Day 16a

---
+++ Solutions/2020/16/a.py
@@ -0,0 +1,30 @@
+# August 6th, 2021
+
+file = open(f"{__file__}/../input.txt", "r")
+
+ranges = []
+line = file.readline().strip()
+while line != "":
+	s = line.split(": ")[1].split(" or ")
+	r = [ x.split('-') for x in s ]
+	r = [ (int(x[0]), int(x[1])) for x in r ]
+	ranges.append(r)
+	line = file.readline().strip()
+file.readline()
+file.readline()
+file.readline()
+file.readline()
+
+s = 0
+for line in file:
+	ticket = [ int(x) for x in line.split(',') ]
+	for x in ticket:
+		valid = False
+		for r in ranges:
+			if (x >= r[0][0] and x <= r[0][1]) or (x >= r[1][0] and x <= r[1][1]):
+				valid = True
+				break
+		if not valid: s += x
+file.close()
+
+print(s)

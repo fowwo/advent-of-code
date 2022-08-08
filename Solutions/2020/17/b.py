57e886a096345d07185598cbd204e86c306020e2
fowwo <54595444+fowwo@users.noreply.github.com>
2022-08-08 11:42:28 -0500

    Solve 2020 Day 17b

---
+++ Solutions/2020/17/b.py
@@ -0,0 +1,38 @@
+# August 8th, 2022
+
+file = open(f"{__file__}/../input.txt", "r")
+
+active = set()
+i = 0
+for line in file:
+	j = 0
+	for c in line.strip():
+		if c == '#': active.add((i, j, 0, 0))
+		j += 1
+	i += 1
+file.close()
+
+def activeNeighbors(x, y, z, w):
+	c = 0
+	for i in range(-1, 2):
+		for j in range(-1, 2):
+			for k in range(-1, 2):
+				for l in range(-1, 2):
+					if i == j == k == l == 0: continue
+					if (x + i, y + j, z + k, w + l) in active:
+						c += 1
+	return c
+
+width = i
+for cycle in range(6):
+	new = set()
+	for i in range(-1 - cycle, 1 + width + cycle):
+		for j in range(-1 - cycle, 1 + width + cycle):
+			for k in range(-1 - cycle, 1 + width + cycle):
+				for l in range(-1 - cycle, 1 + width + cycle):
+					v = activeNeighbors(i, j, k, l)
+					if v == 3 or ((i, j, k, l) in active and v == 2):
+						new.add((i, j, k, l))
+	active = new
+
+print(len(active))

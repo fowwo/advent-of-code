7c385173f606dab3215dff93117358f103e40d33
fowwo <54595444+fowwo@users.noreply.github.com>
2022-12-03 23:11:54 -0600

    Solve 2022 Day 4

---
+++ Solutions/2022/4/a.py
@@ -0,0 +1,13 @@
+# December 3rd, 2022
+
+file = open(f"{__file__}/../input.txt", "r")
+
+s = 0
+for line in file.readlines():
+	line = line.strip().split(',')
+	a = [ int(x) for x in line[0].split('-') ]
+	b = [ int(x) for x in line[1].split('-') ]
+	x = sorted(( a, b ))
+	if x[1][1] <= x[0][1] or x[0][0] == x[1][0]:
+		s += 1
+print(s)

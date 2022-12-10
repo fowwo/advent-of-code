9c0eea3ba02f09e01f193f1a66d598cdfaf70e7f
fowwo <54595444+fowwo@users.noreply.github.com>
2022-12-10 00:12:54 -0600

    Solve 2022 Day 7a

---
+++ Solutions/2022/7/a.py
@@ -0,0 +1,45 @@
+# December 10th, 2022
+
+file = open(f"{__file__}/../input.txt", "r")
+
+path = [ "/" ]
+size = dict()
+nest = dict()
+
+line = file.readline().strip()
+while line:
+	args = line.split(" ")
+	if args[1] == "cd":
+		if args[2] == "/":
+			path = [ "/" ]
+		elif args[2] == "..":
+			path.pop()
+		else:
+			path.append(args[2])
+		line = file.readline().strip()
+	else: # ls
+		s = 0
+		ns = set()
+		line = file.readline().strip()
+		while line and not line.startswith("$"):
+			args = line.split(" ")
+			if args[0] == "dir":
+				ns.add(args[1])
+			else:
+				s += int(args[0])
+			line = file.readline().strip()
+		size["/".join(path)] = s
+		nest["/".join(path)] = ns
+
+def getSize(x):
+	s = size[x]
+	for y in nest[x]:
+		s += getSize(f"{x}/{y}")
+	return s
+
+s = 0
+for d in size:
+	v = getSize(d)
+	if v <= 100000:
+		s += v
+print(s)

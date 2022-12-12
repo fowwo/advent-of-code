43ec5a4c2d3ac8dd1f70161194d1c764f430b05b
fowwo <54595444+fowwo@users.noreply.github.com>
2022-12-11 23:25:58 -0600

    Solve 2022 Day 12

---
+++ Solutions/2022/12/b.py
@@ -0,0 +1,35 @@
+# December 11th, 2022
+
+file = open(f"{__file__}/../input.txt", "r")
+
+grid = dict()
+m = []
+E = None
+
+i = 0
+for line in file:
+	line = line.strip()
+	for j in range(len(line)):
+		grid[(i, j)] = line[j]
+		if line[j] == "a":
+			m.append((i, j))
+		elif line[j] == "E":
+			E = (i, j)
+	i += 1
+
+def height(x):
+	return "SabcdefghijklmnopqrstuvwxyzE".find(grid[(x[0], x[1])])
+
+memo = set()
+steps = 0
+while E not in m:
+	new = []
+	for x in m:
+		for d in [ (-1, 0), (0, -1), (1, 0), (0, 1) ]:
+			y = (x[0] + d[0], x[1] + d[1])
+			if y in grid and y not in memo and height(y) - height(x) <= 1:
+				new.append(y)
+				memo.add(y)
+	m = new
+	steps += 1
+print(steps)

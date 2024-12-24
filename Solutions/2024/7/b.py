e10525631ff4a3bdaba53f9a3b5dec556af5bba2
key <54595444+fowwo@users.noreply.github.com>
2024-12-23 18:09:01 -0600

    Solve 2024 Day 7
    Part 1: 3:39
    Part 2: 4:40

---
+++ Solutions/2024/7/b.py
@@ -0,0 +1,25 @@
+# December 23rd, 2024
+
+file = open(f"{__file__}/../input.txt", "r")
+
+def f(target, nums):
+	if len(nums) == 1: return nums[0] == target
+	x, y = nums.pop(0), nums[0]
+	nums[0] = x + y
+	if f(target, list(nums)): return True
+	nums[0] = x * y
+	if f(target, list(nums)): return True
+	nums[0] = int(str(x) + str(y))
+	if f(target, list(nums)): return True
+	return False
+
+n = 0
+for line in file:
+	a, b = line.strip().split(": ")
+	target = int(a)
+	nums = list(map(int, b.split(" ")))
+	if f(target, nums):
+		n += target
+file.close()
+
+print(n)
